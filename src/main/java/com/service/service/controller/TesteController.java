c48eaadc0249241e2e6187f8d1d19bf130b9abc0
borbinhaa <gabrieldeborba07@gmail.com>
2024-09-26 11:30:23 -0300

    Initial commit

---
+++ src/main/java/com/service/service/controller/TesteController.java
@@ -0,0 +1,15 @@
+package com.service.service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+
+    @GetMapping("teste")
+    public ResponseEntity<String> teste() {
+        return ResponseEntity.ok("teste");
+    }
+}
