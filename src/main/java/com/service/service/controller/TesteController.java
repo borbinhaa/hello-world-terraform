7c392468a87002f798aa2a0353822ba6c4414d56
borbinhaa <gabrieldeborba07@gmail.com>
2024-10-08 15:07:02 -0300

    Create new API

--- src/main/java/com/service/service/controller/TesteController.java
+++ src/main/java/com/service/service/controller/TesteController.java
@@ -7,23 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TesteController {
 
-    @GetMapping("teste")
-    public ResponseEntity<String> teste() {
-        return ResponseEntity.ok("teste");
-    }
 
-    @GetMapping("hello-world")
+    @GetMapping("my-hello-world")
     public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok("hello-world");
-    }
-
-    @GetMapping("hello-world-2")
-    public ResponseEntity<String> helloWorld2() {
-        return ResponseEntity.ok("hello-world-2");
-    }
-
-    @GetMapping("hello-world-3")
-    public ResponseEntity<String> helloWorld3() {
-        return ResponseEntity.ok("hello-world-3");
+        return ResponseEntity.ok("my-hello-world");
     }
 }
